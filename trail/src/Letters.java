6af11047d5c2290cbf31891e2ca40146aec3664e
Dor Ben Dov <doribd@gmail.com>
2021-10-15 11:53:06 +0200

    adding more java 17 feature examples

--- trail/src/Letters.java
+++ trail/src/Letters.java
@@ -3,4 +3,13 @@
  */
 public sealed class Letters permits English {
 
+    /**
+     * This method if statement can be replaced with a switch.
+     * @param object
+     */
+    void check(Object object) {
+        if (object instanceof String name) {
+            System.out.println(name);
+        }
+    }
 }
--- trail/src/Main.java
+++ trail/src/Main.java
@@ -4,12 +4,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        if (args != null && args.length > 0)
+        if (args != null && args.length > 0) {
             switch (args[0]) {
                 case null -> System.out.println("Null");
                 case "1" -> System.out.println("1");
                 case "2" -> System.out.println("2");
                 default -> System.out.println("default");
             }
+        }
     }
+
+
 }
